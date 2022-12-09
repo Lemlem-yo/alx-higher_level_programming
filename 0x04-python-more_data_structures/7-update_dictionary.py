10732d1a64e18de6569e14055e54894989f88dde
Lemlem-yo <ltsifete@gmail.com>
2022-12-09 11:25:05 +0300

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function that replace or add key/value in a dictionary
+"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary

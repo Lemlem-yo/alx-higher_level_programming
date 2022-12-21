24255f95df8a97a6c01c01c3687b64b1b01e0df1
Lemlem-yo <ltsifete@gmail.com>
2022-12-22 02:20:13 +0300

    task 3 correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+""" a sqaure class"""
+
+
 class Square:
-    """type class square"""
+    """ a class that defines private instance variables"""
+    _Square__size = None
+
     def __init__(self, size):
-        """Init the square class
-        Args:
-        param1: size is the type int attribute to make it private
-        """
-        self.__size = size
+
+        """The constructor of the class"""
+        self._Square__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-Square = __import__('3-square').Square
 
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
+""" a sqaure class"""
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
+class Square:
+    """ a class that defines private instance variables"""
 
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
+    _Square__size = None
+
+    def __init__(self, size=0):
+
+        """The constructor of the class"""
+
+        self._Square__size = size
+        if not isinstance(size, int):
+            raise (TypeError("size must be an integer"))
+        if size < 0:
+            raise (ValueError("size must be >= 0"))
+
+    def area(self):
+        return self._Square__size ** 2

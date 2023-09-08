aeb6ee6e9f61915d6210cba1c008b913da5fc1cd
Lemlem-yo <ltsifete@gmail.com>
2023-09-08 22:00:35 +0300

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def producer(hidden_4):
+    for i in dir():
+        if not(i[0] == '-' and i[1] == '-'):
+            print(i)
+
+
+if __name__ == "__main__":
+    producer()

ff3b093a37345847100087d0d206aed1fdf05f70
Lemlem-yo <ltsifete@gmail.com>
2023-09-13 16:37:27 +0300

    task 7

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_key = sorted(a_dictionary)
-
-    for key in sorted_key:
-        print(key, " : ", a_dictionary[key])
+    if a_dictionary is not None:
+        for key, value in sorted(a_dictionary.items()):
+            print(key, end=": ")
+            print(value)
+    else:
+        return

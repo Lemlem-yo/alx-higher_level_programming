9166e135fb113a4461fac5b07003602186ba3c25
Lemlem-yo <ltsifete@gmail.com>
2023-09-26 22:55:52 +0300

    task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        res = fct()
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        res = None
+     finally:
+         return res

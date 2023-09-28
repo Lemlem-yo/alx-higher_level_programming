438814ea7f66d4285d61319a5571a804fb271661
Lemlem-yo <ltsifete@gmail.com>
2023-09-28 22:10:15 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""instantition of square size"""
+
+
+class Square:
+    """
+    instantiion of square size
+
+    Attribute:
+    size
+    """
+    def __init__(self, size):
+        self.__size = size

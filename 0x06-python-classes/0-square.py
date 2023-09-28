f1886b9e2a808dd09b8c74edda13af7a0f3526b9
Lemlem-yo <ltsifete@gmail.com>
2023-09-28 18:15:58 +0300

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 class Square:
     """
-        class square: empty class that define square
+    class square empty class that define square
+
+    Attributes:
+        None
     """
     pass

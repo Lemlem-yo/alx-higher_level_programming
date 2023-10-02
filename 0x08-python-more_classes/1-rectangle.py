6b776053287f6bdbf1eb73d94d5774be0b8daefc
Lemlem-yo <ltsifete@gmail.com>
2023-10-02 22:05:05 +0300

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-
+        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -29,7 +29,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-
+        """height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:

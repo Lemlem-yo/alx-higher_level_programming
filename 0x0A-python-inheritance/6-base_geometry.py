d35f72e3fc37c1aab9445f5505ed0980ef1ab84f
Lemlem-yo <ltsifete@gmail.com>
2023-10-09 16:10:14 +0300

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write class basegeometry"""
+
+
+class BaseGeometry:
+    """modify using fuction"""
+    def area(self):
+        raise Exception("area() is not implemented")

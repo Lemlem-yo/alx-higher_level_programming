257920fa89f0bb4cb79741adb13f17c23210bd5b
Lemlem-yo <ltsifete@gmail.com>
2023-10-11 15:20:14 +0300

    task 7 it very hard for me

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""add_item methon"""
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, "add_item.json")

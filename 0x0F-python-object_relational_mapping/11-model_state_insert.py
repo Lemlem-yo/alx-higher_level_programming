b728271db6aa5869d5c31476cdbc4c06f47905f6
Lemlem-yo <ltsifete@gmail.com>
2023-12-17 15:11:42 +0300

    task

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,46 +1,27 @@
 #!/usr/bin/python3
-"""
-Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa
-"""
+"""script that adds the State object “Louisiana”"""
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    usern, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Create a SQLAlchemy engine
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, database),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(usern, passwd, db), pool_pre_ping=True)
 
-    # Create a session factory
     Session = sessionmaker(bind=engine)
-
-    # Create a session
     session = Session()
 
-    # Create a new State object
     new_state = State(name="Louisiana")
-
-    # Add the new State object to the session
     session.add(new_state)
-
-    # Commit the changes to the database
     session.commit()
 
-    # Print the new states.id after creation
     print(new_state.id)
-
-    # Close the session
     session.close()
-
--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
-"""
-Script that changes the name of a State object in the database hbtn_0e_6_usa
-"""
+"""script that changes the name of a State"""
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    usern, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Create a SQLAlchemy engine
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, database),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(usern, passwd, db), pool_pre_ping=True)
 
-    # Create a session factory
     Session = sessionmaker(bind=engine)
-
-    # Create a session
     session = Session()
 
-    # Query the State object with id = 2
+    # Retrieve the State object with id 2 and update its name
     state_to_update = session.query(State).filter(State.id == 2).first()
-
-    # Check if the State object exists
     if state_to_update:
-        # Change the name to "New Mexico"
         state_to_update.name = "New Mexico"
-
-        # Commit the changes to the database
         session.commit()
 
-    # Close the session
     session.close()

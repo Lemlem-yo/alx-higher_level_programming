8614dfa1a58ad1dac59425019807e367dd6ee365
Lemlem-yo <ltsifete@gmail.com>
2023-12-17 15:49:56 +0300

    task

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
-"""
-Display all cities of a given state from the 
-states table of the database hbtn_0e_4_usa.
-Safe from SQL injections.
-Usage: ./5-filter_cities.py <mysql username> \
-                            <mysql password> \
-                            <database name> \
-                            <state name searched>
-"""
-import sys
+'''write a script that takes in arguments and displays'''
 import MySQLdb
+import sys
+
+
+def main():
+    # makes conn and executes query
+    # Retrieve command-line arguments
+    username, password, database, state_name = sys.argv[1],\
+        sys.argv[2], sys.argv[3], sys.argv[4]
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+    # Create a cursor object
+    cursor = db.cursor()
+    # Execute the SQL query to select states with a matching name
+    query = """
+        SELECT cities.name FROM cities JOIN states ON\
+        cities.state_id = states.id AND states.name = %s ORDER BY cities.id ASC
+        """
+    cursor.execute(query, (state_name,))
+    # Fetch all the results
+    result = cursor.fetchall()
+    ll = [x[0] for x in result]
+    total = ", ".join(ll)
+    print(total)
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-     print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    from sys import argv
+    main()
